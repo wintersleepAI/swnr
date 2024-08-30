527001f3141aa9c4d5458916dbb698a284721306
wintersleepAI <wintersleep@protonmail.com>
2024-08-29 23:09:57 -0500

    removed moved files

--- module/data/item-feature.mjs
+++
@@ -1,8 +0,0 @@
-import SWNItemBase from './base-item.mjs';
-
-export default class SWNFeature extends SWNItemBase {
-  static LOCALIZATION_PREFIXES = [
-    'SWN.Item.base',
-    'SWN.Item.Feature',
-  ];
-}
