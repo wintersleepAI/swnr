fe37acfe0744efc3d785837adae6a4374d777e37
wintersleepAI <wintersleep@protonmail.com>
2024-08-30 10:47:58 -0500

    moved

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -1,10 +1,10 @@
 // Export Actors
-export { default as SWNActorBase } from './base-actor.mjs';
-export { default as SWNCharacter } from './actor-character.mjs';
-export { default as SWNNPC } from './actor-npc.mjs';
+export { default as SWNActorBase } from './actors/base-actor.mjs';
+export { default as SWNCharacter } from './actors/actor-character.mjs';
+export { default as SWNNPC } from './actors/actor-npc.mjs';
 
 // Export Items
-export { default as SWNItemBase } from './base-item.mjs';
-export { default as SWNGear } from './item-gear.mjs';
-export { default as SWNFeature } from './item-feature.mjs';
-export { default as SWNSpell } from './item-spell.mjs';
+export { default as SWNItemBase } from './items/base-item.mjs';
+export { default as SWNGear } from './items/item-gear.mjs';
+export { default as SWNFeature } from './items/item-feature.mjs';
+export { default as SWNSpell } from './items/item-spell.mjs';
