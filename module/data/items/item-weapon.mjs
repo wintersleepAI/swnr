aea046db99622617b8679779888ddfed70c3d057
wintersleepAI <wintersleep@protonmail.com>
2024-10-31 23:07:47 -0500

    roll attack form working in theory

--- module/data/items/item-weapon.mjs
+++ module/data/items/item-weapon.mjs
@@ -67,6 +67,16 @@ export default class SWNWeapon extends SWNBaseGearItem {
     );
   }
 
+  async rollAttack(
+    damageBonus, // number
+    stat, // number
+    skillMod, // number
+    modifier, // number
+    useBurst // boolean
+  ) {
+    alert("Attacking ! " + damageBonus + " " + stat + " " + skillMod + " " + modifier + " " + useBurst);  
+  }
+
   async roll(shiftKey = false) {
     let item = this.parent;
     const actor = item.actor;
@@ -105,11 +115,125 @@ export default class SWNWeapon extends SWNBaseGearItem {
       statName = secStatName;
     }
 
-    await roll.toMessage({
-      speaker: ChatMessage.getSpeaker({alias: this.parent.name}),
-      flavor: `${actor.name}'s ${this.parent.name} Damage Roll`,
+    // Set to not ask and just roll
+    if (!shiftKey && this.remember && this.remember.use) {
+      const stat = actor["stats"]?.[statName] || {
+        mod: 0,
+      };
 
+      const skill = actor.getEmbeddedDocument(
+        "Item",
+        this.skill
+      );
+      const skillMod = skill.rank < 0 ? -2 : skill.rank;
+
+      if (actor?.type == "character") {
+        dmgBonus = this.skillBoostsDamage ? skill.rank : 0;
+      }
+      return this.rollAttack(
+        dmgBonus,
+        stat.mod,
+        skillMod,
+        this.remember.modifier,
+        this.remember.burst
+      );
+    }
+
+    const dialogData = {
+      actor: actor,
+      weapon: this,
+      skills: actor.itemTypes.skill,
+      statName: statName,
+      skill: this.skill,
+      burstFireHasAmmo,
+    };
+    const template = "systems/swnr/templates/dialogs/roll-attack.hbs";
+    const html = await renderTemplate(template, dialogData);
+
+    const _rollForm = async (_event, button, html) => {
+      const modifier = parseInt(button.form.elements.modifier.value);
+      const burstFire = (button.form.elements.burstFire?.checked) ? true : false;
+      const skillId = button.form.elements.skill.value || this.skill;
+
+      if (!actor) {
+        console.log("Error actor no longer exists ");
+        return;
+      }
+      let skillMod = 0;
+
+      const skill = actor.getEmbeddedDocument(
+        "Item",
+        skillId
+      );
+
+      if (actor?.type == "npc" && html.find('[name="skilled"]')) {
+        const npcSkillMod = button.form.elements.skilled?.checked ? actor.system.skillBonus : 0;
+        if (npcSkillMod) skillMod = npcSkillMod;
+      } else {
+        skillMod = skill.system.rank < 0 ? -2 : skill.system.rank;
+      }
+      // for finesse weapons take the stat with the higher mod
+      let statName = this.stat;
+      const secStatName = this.secondStat;
+      // check if there is 2nd stat name and its mod is better
+      if (
+        secStatName != null &&
+        secStatName != "none" &&
+        actor.system.stats[statName].mod <
+          actor.system.stats[secStatName].mod
+      ) {
+        statName = secStatName;
+      }
+
+      const stat = actor.system.stats?.[statName] || {
+        mod: 0,
+      };
+      // 1d20 + attack bonus (PC plus weapon) + skill mod (-2 if untrained)
+      // weapon dice + stat mod + skill if enabled or punch.
+      // shock: damage + stat
+      // const skill = actor.items.filter(w => w.)
+      // Burst is +2 To hit and to damage
+      if (actor?.type == "character") {
+        dmgBonus = this.skillBoostsDamage ? skill.system.rank : 0;
+      } else if (actor?.type == "npc") {
+        dmgBonus = this.skillBoostsDamage
+          ? actor.system.skillBonus
+          : 0;
+        if (actor.system.attacks.bonusDamage) {
+          dmgBonus += actor.system.attacks.bonusDamage;
+        }
+      }
+      // If remember is checked, set the skill and data
+      const remember = (button.form.elements.remember?.checked) ? true : false;
+      if (remember) {
+        await this.update({
+          system: {
+            remember: {
+              use: true,
+              burst: burstFire,
+              modifier: modifier,
+            },
+            skill: skillId,
+          },
+        });
+      }
+
+      return this.rollAttack(dmgBonus, stat.mod, skillMod, modifier, burstFire);
+      // END roll form
+    };
+
+    const attackDialog = await foundry.applications.api.DialogV2.wait({
+      window: { title: title },
+      content: html,
+      modal: false,
+      rejectClose: false,
+      buttons: [
+        {
+          label: game.i18n.localize("swnr.chat.roll"),
+          callback: _rollForm,
+        },
+      ],
     });
-    return roll;
+
   }
 }
