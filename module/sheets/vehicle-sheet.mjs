05a3ead8ef8473a90c5e1927d36ae18d90963e63
wintersleepAI <wintersleep@protonmail.com>
2024-12-16 23:12:31 -0600

    stipped ts code, not working

--- module/sheets/vehicle-sheet.mjs
+++ module/sheets/vehicle-sheet.mjs
@@ -494,14 +494,14 @@ export class SWNVehicleSheet extends api.HandlebarsApplicationMixin(
    * @param {DragEvent} event       The originating DragEvent
    * @protected
    */
-  _onDragOver(event) {}
+  _onDragOver(event, target) {}
 
   /**
    * Callback actions which occur when a dragged element is dropped on a target.
    * @param {DragEvent} event       The originating DragEvent
    * @protected
    */
-  async _onDrop(event) {
+  async _onDrop(event, target) {
     const data = TextEditor.getDragEventData(event);
     const actor = this.actor;
     const allowed = Hooks.call('dropActorSheetData', actor, this, data);
@@ -736,8 +736,600 @@ export class SWNVehicleSheet extends api.HandlebarsApplicationMixin(
     });
   }
 
+  // Add Currency to selected type
+async _onAddCurrency(event, target) {
+  // Stop propagation
+  event.preventDefault();
+  event.stopPropagation();
 
+  // get any useful params
+  const currencyType = $(event.currentTarget).data("creditType");
+
+  // load html variable data for dialog
+  const template = "systems/swnr/templates/dialogs/add-currency.html";
+  const data = {};
+  const html = await renderTemplate(template, data);
+  this.popUpDialog?.close();
   
+  // show a dialog prompting for amount of change to add
+  const amount = await new Promise((resolve) => {
+    new ValidatedDialog({
+      title: game.i18n.localize("swnr.AddCurrency"),
+      content: html,
+      buttons: {
+        one: {
+          label: "Add",
+          callback: (html) => resolve(html.find('[name="amount"]').val()),
+        },
+        two: {
+          label: "Cancel",
+          callback: () => resolve("0"),
+        },
+      },
+      default: "one",
+      close: () => resolve("0")
+    },{
+      classes: ["swnr"]
+    }).render(true);
+  });
+
+  // If it's not super easily parsable as a number
+  if (isNaN(parseInt(await amount))) {
+    ui.notifications?.error(game.i18n.localize("swnr.InvalidNumber"));
+    return;
+  }
+
+  // if the amount is 0, or the cancel button was hit
+  if (parseInt(amount) == 0) {
+    // we can return silently in this case
+    return;
+  } else {
+    // this is our valid input scenario
+    await this.actor.update({
+      data: {
+        [currencyType]: this.actor.data.data[currencyType] + parseInt(await amount)
+      }
+    });
+  }
+}
+
+async _onResourceName(event, target) {
+  event.preventDefault();
+  event.stopPropagation();
+  const value = target?.value;
+  const resourceType = $(event.currentTarget).data("rlType");
+  const idx = $(event.currentTarget).parents(".item").data("rlIdx");
+  const resourceList = duplicate(this.actor.data.data.cargoCarried);
+  resourceList[idx][resourceType] = value;
+  await this.actor.update({ "data.cargoCarried": resourceList });
+}
+
+async _onResourceDelete(event, target) {
+  event.preventDefault();
+  event.stopPropagation();
+  const idx = $(event.currentTarget).parents(".item").data("rlIdx");
+  const resourceList = duplicate(this.actor.data.data.cargoCarried);
+  resourceList.splice(idx, 1);
+  await this.actor.update({ "data.cargoCarried": resourceList });
+}
+
+async _onResourceCreate(event, target) {
+  event.preventDefault();
+  // console.log("Changing HP Max", this.actor);
+  let resourceList = this.actor.data.data.cargoCarried;
+  if (!resourceList) {
+    resourceList = [];
+  }
+  resourceList.push({ name: "Cargo X", value: 0, max: 1 });
+  await this.actor.update({
+    "data.cargoCarried": resourceList,
+  });
+}
+
+async _onPayment(event, target) {
+  return this._onPay(event, "payment");
+}
+
+async _onMaintenance(event, target) {
+  return this._onPay(event, "maintenance");
+}
+
+async _onPay(event, paymentType) {
+  // if a new payment type is added this function needs to be refactored
+  let shipPool = this.actor.data.data.creditPool;
+  const paymentAmount = 
+    paymentType == "payment"
+      ? this.actor.data.data.paymentAmount
+      : this.actor.data.data.maintenanceCost;
+  // Assume its payment and change if maintenance
+
+  const lastPayDate =
+    paymentType == "payment"
+      ? this.actor.data.data.lastPayment
+      : this.actor.data.data.lastMaintenance;
+  const monthSchedule =
+    paymentType == "payment"
+      ? this.actor.data.data.paymentMonths
+      : this.actor.data.data.maintenanceMonths;
+
+  if (paymentAmount > shipPool) {
+    ui.notifications?.error(
+      `Not enough money in the pool for paying ${paymentAmount} for ${paymentType}`
+    );
+    return;
+  }
+  shipPool -= paymentAmount;
+  const dateObject = new Date(
+    lastPayDate.year,
+    lastPayDate.month - 1,
+    lastPayDate.day
+  );
+  dateObject.setMonth(dateObject.getMonth() + monthSchedule);
+  if (paymentType == "payment") {
+    await this.actor.update({
+      data: {
+        creditPool: shipPool,
+        lastPayment: {
+          year: dateObject.getFullYear(),
+          month: dateObject.getMonth() + 1,
+          day: dateObject.getDate(),
+        },
+      },
+    });
+  } else {
+    await this.actor.update({
+      data: {
+        creditPool: shipPool,
+        lastMaintenance: {
+          year: dateObject.getFullYear(),
+          month: dateObject.getMonth() + 1,
+          day: dateObject.getDate(),
+        },
+      },
+    });
+  }
+  this.actor.setScheduledDate(dateObject, paymentType);
+}
+
+async _onHullChange(event, target) {
+  const targetHull = event.target?.value;
+
+  if (targetHull) {
+    const d = new Dialog({
+      title: "Apply Default Stats",
+      content: `<p>Do you want to apply the default stats for a ${targetHull}?</p><b>This will change your current and max values for HP, cost, armor, AC, mass, power, hardpoints, hull type, speed, life support (60*max crew), and crew.</b>`,
+      buttons: {
+        yes: {
+          icon: '<i class="fas fa-check"></i>',
+          label: "Yes",
+          callback: () => this.actor.applyDefaulStats(targetHull),
+        },
+        no: {
+          icon: '<i class="fas fa-times"></i>',
+          label: "No",
+          callback: () => {
+            console.log("Doing Nothing ");
+          },
+        },
+      },
+      default: "no",
+    });
+    d.render(true);
+  }
+}
+
+async _onRepair(event, target) {
+  event.preventDefault();
+  const data = this.actor.data.data;
+  const hpToFix = data.health.max - data.health.value;
+  const hpCosts = hpToFix * 1000;
+  const shipInventory = this.actor.items.filter(
+    (i) =>
+      i.type === "shipDefense" ||
+      i.type === "shipWeapon" ||
+      i.type === "shipFitting"
+  );
+
+  const disabledParts = shipInventory.filter(
+    (i) => i.data.data.broken == true && i.data.data.destroyed == false
+  );
+  const shipClass = this.actor.data.data.shipClass;
+
+  let multiplier = 1;
+  if (shipClass == "frigate") {
+    multiplier = 10;
+  } else if (shipClass == "cruiser") {
+    multiplier = 25;
+  } else if (shipClass == "capital") {
+    multiplier = 100;
+  }
+
+  let disabledCosts = 0;
+  const itemsToFix = [];
+  const eitems = [];
+  for (let i = 0; i < disabledParts.length; i++) {
+    const item = disabledParts[i];
+    const itemCost = item.data.data.costMultiplier
+      ? item.data.data.cost * multiplier
+      : item.data.data.cost;
+    disabledCosts += itemCost;
+    itemsToFix.push(`${item.name} (${itemCost})`);
+    eitems.push({ _id: item.id, data: { broken: false } });
+  }
+  if (eitems.length > 0) {
+    await this.actor.updateEmbeddedDocuments("Item", eitems);
+  }
+  const fullRepairCost = disabledCosts * 0.25;
+  const totalCost = hpCosts + fullRepairCost;
+  await this.actor.update({ "data.health.value": data.health.max });
+  if (totalCost > 0) {
+    const itemList = itemsToFix.join("<br>");
+    const content = `<h3>Ship Repaired</h3><b>Estimated Total Cost: ${totalCost}</b><br>HP points: ${hpToFix} cost: ${hpCosts}<br>Full Repair Costs: ${fullRepairCost} (25%/item cost). <br><br> Items Repaired (item full cost):<br> ${itemList} `;
+    const chatData = {
+      content: content,
+    };
+    ChatMessage.create(chatData);
+  }
+}
+
+async _onTravel(event, target) {
+  event.preventDefault();
+  if (this.actor.data.data.spikeDrive.value <= 0) {
+    ui.notifications?.error("Drive disabled.");
+    return;
+  }
+  // TODO localize
+  new Dialog({
+    title: "Travel Days (Use life support)",
+    content: `
+        <form>
+          <div class="form-group">
+            <label>Days of Travel</label>
+            <input type='text' name='inputField'></input>
+          </div>
+        </form>`,
+    buttons: {
+      yes: {
+        icon: "<i class='fas fa-check'></i>",
+        label: `Travel`,
+      },
+    },
+    default: "yes",
+    close: (html) => {
+      const form = html[0].querySelector("form");
+      const days = form.querySelector('[name="inputField"]')?.value;
+      if (days && days != "") {
+        const nDays = Number(days);
+        if (nDays) {
+          this.actor.useDaysOfLifeSupport(nDays);
+          if (game.modules?.get("foundryvtt-simple-calendar")?.active) {
+            this.actor.moveTime(nDays);
+          }
+        }
+      }
+    },
+  }).render(true);
+}
+
+
+async _onCrewNPCRoll(event, target) {
+  event.preventDefault();
+  // Roll skill, show name, skill, attr if != ""
+  const rollMode = game.settings.get("core", "rollMode");
+  const formula = `2d6 + @npcCrewSkill`;
+  const npcCrewSkill = this.actor.data.data.crewSkillBonus
+    ? this.actor.data.data.crewSkillBonus
+    : 0;
+  const roll = new Roll(formula, {
+    npcCrewSkill,
+  });
+  await roll.roll({ async: true });
+  const title = `Rolling generic skill with bonus ${npcCrewSkill}`;
+  roll.toMessage(
+    {
+      speaker: ChatMessage.getSpeaker(),
+      flavor: title,
+    },
+    { rollMode }
+  );
+}
+
+async _setCaptSupport(dept) {
+  const deptSupport = this.actor.data.data.supportingDept
+    ? this.actor.data.data.supportingDept
+    : "";
+  if (dept == deptSupport) {
+    return;
+  }
+  if (deptSupport != "") {
+    ui.notifications?.error("Department is already supported. Error.");
+  }
+  await this.actor.update({
+    "data.supportingDept": dept,
+  });
+}
+
+async _onShipAction(event, target) {
+  event.preventDefault();
+  const actionName = event.target?.value;
+  if (actionName === "") {
+    return;
+  }
+  const action = ACTIONS[actionName];
+  if (!action) {
+    ui.notifications?.error("There was an error in looking up your action");
+    return;
+  }
+  const actionTitle = action.title ? action.title : actionName;
+  let cp = this.actor.data.data.commandPoints;
+  const actionsTaken = this.actor.data.data.actionsTaken
+    ? this.actor.data.data.actionsTaken
+    : [];
+  if (action.limit === "round" && actionsTaken.indexOf(actionName) >= 0) {
+    ui.notifications?.info(
+      "Already taken the action this round. Did you forget to end the last round?"
+    );
+    return;
+  }
+  // Special actions
+  if (actionName == "startRound") {
+    let depts = ``;
+    let roleOrder = [];
+    if (this.actor.data.data.roleOrder) {
+      // we may have set an order prior
+      roleOrder = this.actor.data.data.roleOrder;
+    } else {
+      // get the default list
+      for (const role in this.actor.data.data.roles) {
+        roleOrder.push(role);
+      }
+    }
+    for (const role of roleOrder) {
+      let roleName = "";
+      if (this.actor.data.data.roles[role]) {
+        const roleActor = game.actors?.get(this.actor.data.data.roles[role]);
+        if (roleActor && roleActor.name) {
+          roleName = ` (${roleActor.name})`;
+        }
+      }
+      depts += `<div class="border p-2 flex border-black role-order" data-role="${role}" data-role-name="${roleName}"><a><i class="fas fa-sort"></i></a>${role}${roleName}</div>`;
+    }
+    const dialogTemplate = `
+    <div class="flex flex-col -m-2 p-2 pb-4 space-y-2">
+      <h1> Order Departments/Roles </h1>
+      <div class="flex flexrow">
+          <div id="deptOrder">
+          ${depts}
+          </div>
+      </div>
+      <script>
+      var el = document.getElementById('deptOrder');
+      var sortable = Sortable.create(el);
+      </script>
+    </div>
+    `;
+    new Dialog(
+      {
+        title: "Set Order",
+        content: dialogTemplate,
+        buttons: {
+          setOrder: {
+            label: "Set Order",
+            callback: async (html) => {
+              const order = html.find("#deptOrder");
+              const orderArr = [];
+              order.children(".role-order").each(function () {
+                orderArr.push($(this).data("role"));
+              });
+              if (orderArr.length > 0) {
+                await this.actor.update({ data: { roleOrder: orderArr } });
+              }
+            },
+          },
+          close: {
+            label: "Close",
+          },
+        },
+        default: "setOrder",
+      },
+      { classes: ["swnr"] }
+    ).render(true);
+    return;
+  } else if (actionName == "endRound") {
+    // endRound action is special. clear and reset.
+    const newCp = this.actor.data.data.npcCommandPoints
+      ? this.actor.data.data.npcCommandPoints
+      : 0;
+    let actionsText =
+      actionsTaken.length > 0 ? `Actions: <ul>` : "No actions.";
+
+    if (actionsTaken.length > 0) {
+      for (const act of actionsTaken) {
+        const actTitle =
+          ACTIONS[act] && ACTIONS[act].title ? ACTIONS[act].title : act;
+        actionsText += `<li>${actTitle}</li>`;
+      }
+      actionsText += "</ul>";
+    }
+    const chatData = {
+      content: `Round ended for ${this.actor.name}. Setting CP to ${newCp}<br>${actionsText}`,
+    };
+    actionsTaken.length = 0;
+    await this.actor.update({
+      data: {
+        commandPoints: newCp,
+        actionsTaken: actionsTaken,
+        supportingDept: "",
+      },
+    });
+    event.target.value = "";
+    ChatMessage.create(chatData);
+    return;
+  }
+  let actionCp = action.cp ? action.cp : 0;
+  let supported = false;
+  let supportingDept = this.actor.data.data.supportingDept;
+  if (action.dept && action.dept == supportingDept) {
+    // If captain is supporting the department.
+    actionCp += 2;
+    supported = true;
+  }
+  // Verify enough CP
+  if (actionCp < 0 && cp + actionCp < 0) {
+    ui.notifications?.error("Not enough command points");
+    return;
+  }
+  const noteText = action.note ? action.note : "";
+  let diffText = action.dc ? `<br>Difficulty:${action.dc}` : "";
+  if (action.dc === "opposed") {
+    const res = await new Roll("2d6").roll();
+    diffText += ` (2d6: ${res.total})`;
+  }
+  const descText = action.desc ? action.desc : "";
+  const order = this.actor.data.data.roleOrder
+    ? ` (${this.actor.data.data.roleOrder.join(",")})`
+    : "";
+  if (action.skill) {
+    // this action needs a skill roll
+    let skillLevel = -1;
+    let attrMod = 0;
+    let attrName = "";
+    let dicePool = "2d6";
+
+    if (action.dept) {
+      let foundActor = false; // might not be anyone in this dept/role
+      if (this.actor.data.data.roles[action.dept] != "") {
+        const defaultActor = game.actors?.get(
+          this.actor.data.data.roles[action.dept]
+        );
+        if (defaultActor) {
+          foundActor = true;
+          if (defaultActor.type == "character") {
+            for (const skill of defaultActor.itemTypes.skill) {
+              if (action.skill == skill.data.name) {
+                skillLevel = skill.data.data["rank"];
+                dicePool =
+                  skill.data.data["pool"] && skill.data.data["pool"] != "ask"
+                    ? skill.data.data["pool"]
+                    : "2d6";
+              }
+            }
+            let tempMod = -2;
+            if (action.attr) {
+              // Find the attribute with the highest mod
+              for (const attr of action.attr) {
+                if (
+                  defaultActor.data.data.stats[attr] &&
+                  defaultActor.data.data.stats[attr].mod > tempMod
+                ) {
+                  tempMod = defaultActor.data.data.stats[attr].mod;
+                  const key = `swnr.stat.short.${attr}`;
+                  attrName = `${game.i18n.localize(key)}\\`;
+                }
+              }
+              attrMod = tempMod;
+            }
+          }
+          if (defaultActor.type == "npc") {
+            skillLevel = defaultActor.data.data.skillBonus;
+          }
+          // Roll skill, show name, skill, attr if != ""
+          const rollMode = game.settings.get("core", "rollMode");
+          const formula = `${dicePool} + @skillLevel + @attrMod`;
+          const roll = new Roll(formula, {
+            skillLevel,
+            attrMod,
+          });
+          await roll.roll({ async: true });
+          const title = `<span title="${descText}">Rolling ${actionTitle} ${attrName}${action.skill} for ${defaultActor.name}<br>${noteText}${diffText}</span><br>${order}`;
+          roll.toMessage(
+            {
+              speaker: ChatMessage.getSpeaker(),
+              flavor: title,
+            },
+            { rollMode }
+          );
+        }
+      }
+      if (!foundActor) {
+        // We are here means there is a skill but we don't know who it is for
+        skillLevel = this.actor.data.data.crewSkillBonus
+          ? this.actor.data.data.crewSkillBonus
+          : 0;
+        const rollMode = game.settings.get("core", "rollMode");
+        const formula = `${dicePool} + @skillLevel`;
+        const roll = new Roll(formula, {
+          skillLevel,
+          attrMod,
+        });
+        await roll.roll({ async: true });
+        const title = `<span title="${descText}">Rolling ${actionTitle} ${attrName}${action.skill}. No PC/NPC set to role/dept.<br>${noteText}${diffText}</span>`;
+        roll.toMessage(
+          {
+            speaker: ChatMessage.getSpeaker(),
+            flavor: title,
+          },
+          { rollMode }
+        );
+      }
+    }
+  } else {
+    if (actionName == "supportDept") {
+      const d = new Dialog({
+        title: "Support Department",
+        content:
+          "<p>You must choose a department to support (actions are 2 CP cheaper)</p>",
+        buttons: {
+          bridge: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Bridge",
+            callback: () => this._setCaptSupport("bridge"),
+          },
+          comms: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Comms",
+            callback: () => this._setCaptSupport("comms"),
+          },
+          engineering: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Eng.",
+            callback: () => this._setCaptSupport("engineering"),
+          },
+          gunnery: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Gunnery",
+            callback: () => this._setCaptSupport("gunnery"),
+          },
+        },
+        default: "bridge",
+      });
+      d.render(true);
+    }
+    // there is no skill
+    const chatData = {
+      content: `<span title="${descText}">${this.actor.name} ${actionTitle}<br><span class="flavor-text message-header" style="font-size:12px;">${noteText}${diffText}</span><br>${order}</span>`,
+    };
+    ChatMessage.create(chatData);
+  }
+  // Consume CP
+  cp += actionCp;
+  actionsTaken.push(actionName);
+  if (supported) {
+    // one-time action
+    supportingDept = "";
+  }
+  await this.actor.update({
+    "data.commandPoints": cp,
+    "data.actionsTaken": actionsTaken,
+    "data.supportingDept": supportingDept,
+  });
+  event.target.value = "";
+  return;
+}
+
+
+
+
 
   /********************
    *
