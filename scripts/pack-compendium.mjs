f67baabfd65e3897d6d943539045c5a590a6f8ec
Young Jae Ahn <youngjaeahn1995@gmail.com>
2025-01-18 13:59:05 -0600

    Half-finished translating compendium pack

--- scripts/pack-compendium.mjs
+++ scripts/pack-compendium.mjs
@@ -1,25 +1,48 @@
-import { compilePack } from '@foundryvtt/foundryvtt-cli';
-import { promises as fs } from 'fs';
+import { compilePack } from "@foundryvtt/foundryvtt-cli";
+import { promises as fs } from "fs";
+import path from "path";
 
-const MODULE_ID = process.cwd();
+const SYSTEM_ID = process.cwd();
+
+const BASE_SRC_PATH = "src/packs";
+const BASE_DEST_PATH = "packs";
+
+// await compilePacksRecursivly();
+
+/**
+ * Compiles all packs in the given base path
+ */
+async function compilePacksRecursivly() {
+    const packs = (await fs.readdir(BASE_SRC_PATH, { withFileTypes: true })).filter(file => file.isDirectory());
+
+    for (const pack of packs) {
+        if (pack.name === '.gitattributes') continue;
+
+        const srcPath = path.join(BASE_SRC_PATH, pack.name);
+        const destPath = path.join(BASE_DEST_PATH, pack.name);
+        console.log("Packing " + srcPath + " to " + destPath);
+        await compilePack(
+            `${SYSTEM_ID}/${srcPath}`,
+            `${SYSTEM_ID}/${destPath}`,
+            { yaml: true, recursive: true, log: true }
+        );
+    }
+}
+
 const IGNORE = [ '.gitattributes', '.DS_Store' ];
 
-fs.readdir(`${MODULE_ID}/src/packs`, {withFileTypes: true})
-    .filter(file => file.isDirectory())
+fs.readdir(`${SYSTEM_ID}/src/packs`)
     .then(packs => {
         for (const pack of packs) {
+            console.log('pack:', pack);
             if (IGNORE.includes(pack.name)) {
                 continue;
             }
 
-            console.log(`Packing ${pack.name}`);
-            compilePack(
-                `${MODULE_ID}/src/packs/${pack.name}`, 
-                `${MODULE_ID}/packs/${pack.name}`,
-                { yaml: true, recursive: true, log: true }
-            )
-            .catch(err => {
-                console.log(err);
-            });
+            console.log(`Packing ${pack}`);
+            compilePack(`${SYSTEM_ID}/src/packs/${pack}`, `${SYSTEM_ID}/packs/${pack}`, { yaml: true })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     });
--- scripts/unpack-compendium.mjs
+++ scripts/unpack-compendium.mjs
@@ -1,20 +1,124 @@
-import { extractPack } from '@foundryvtt/foundryvtt-cli';
-import { promises as fs } from 'fs';
-import path from 'path';
+import { extractPack } from "@foundryvtt/foundryvtt-cli";
+import { promises as fs } from "fs";
+import path from "path";
 
-const MODULE_ID = process.cwd();
+const SYSTEM_ID = process.cwd();
+const BASE_LDB_PATH = "packs";
+const BASE_SRC_PATH = "src";
+
+// await extractPacks();
+
+/**
+ * Unpacks all compendium packs located in the basePath
+ */
+async function extractPacks() {
+    const dirents = await fs.readdir(BASE_LDB_PATH, { withFileTypes: true, recursive: true });
+    const packs = dirents.filter((dirent) => dirent.isDirectory());
+
+    const folders = {};
+    for (const pack of packs) {
+        const packName = path.join(pack.path, pack.name);
+        console.log("Unpacking " + packName);
+        await extractAllFoldersFromPackFile(folders, packName);
+        Object.values(folders).forEach(folder => buildAndAddPath(folders, folder));
+        await unpackToPath(folders, packName);
+    }
+}
+
+/**
+ * Reads all folders inside a pack and adds it to a collection of folders
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {string} packName - Path to the pack ldb folder
+ */
+async function extractAllFoldersFromPackFile(collection, packName) {
+    await extractPack(
+        `${SYSTEM_ID}/${packName}`,
+        `${SYSTEM_ID}/${BASE_SRC_PATH}/${packName}`,
+        {
+            yaml: true,
+            transformEntry: (entry) => {
+                if (entry._key.startsWith("!folders")) {
+                    collection[entry._id] = { name: slugify(entry.name), parentFolder: entry.folder };
+                }
+                return false;
+            }
+        }
+    );
+}
+
+/**
+ * Adds a path to each folder in the collection
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {object} folderEntry - An entry from the folders collection
+ */
+function buildAndAddPath(collection, folderEntry) {
+    let parent = collection[folderEntry.parentFolder];
+    folderEntry.path = folderEntry.name;
+    while (parent) {
+        folderEntry.path = path.join(parent.path, folderEntry.path);
+        parent = collection[parent.parentFolder];
+    }
+}
+
+/**
+ * Unpacks all files and folder from the given packName path
+ * Files are unpacked into their respective compendium folders
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {string} packName - Path to the pack ldb folder
+ */
+async function unpackToPath(collection, packName) {
+    await extractPack(
+        `${SYSTEM_ID}/${packName}`,
+        `${SYSTEM_ID}/${BASE_SRC_PATH}/${packName}`,
+        {
+            yaml: true,
+            transformName: entry => {
+                const filename = transformName(entry);
+                if (entry._id in collection) {
+                    return path.join(collection[entry._id].path, filename);
+                }
+                const parent = collection[entry.folder];
+                return path.join(parent?.path ?? "", filename);
+            }
+        }
+    );
+}
+
+/**
+ * Prefaces the document with its type
+ * @param {object} doc - The document data
+ */
+function transformName(doc) {
+    const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, "_");
+    const type = doc._key.split("!")[1];
+    const prefix = ["actors", "items"].includes(type) ? doc.type : type;
+
+    return `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.yaml`;
+}
+
+/**
+ * Standardize name format.
+ * @param {string} name
+ * @returns {string}
+ */
+function slugify(name) {
+    return name.toLowerCase().replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
+}
+
+
+
+
 const IGNORE = [ '.gitattributes', '.DS_Store' ];
 
-fs.readdir(`${MODULE_ID}/packs`, {withFileTypes: true, recursive: true})
-    .filter(file => file.isDirectory())
+fs.readdir(`${SYSTEM_ID}/packs`)
     .then(packs => {
         for (const pack of packs) {
-            if (IGNORE.includes(pack.name)) {
+            if (IGNORE.includes(pack)) {
                 continue;
             }
 
             console.log(`Unpacking ${pack}`);
-            const directory = `${MODULE_ID}/src/packs/${pack.name}`;
+            const directory = `${SYSTEM_ID}/src/packs/${pack}`;
 
             // Delete all the pack files in the source directory.
             fs.readdir(directory)
@@ -23,33 +127,31 @@ fs.readdir(`${MODULE_ID}/packs`, {withFileTypes: true, recursive: true})
                         fs.unlink(path.join(directory, file))
                             .catch(err => {
                                 if (err.code === 'ENOENT') {
-                                    console.log(`No files inside of ${pack.name}`);
+                                    console.log(`No files inside of ${pack}`);
                                 } else {
                                     throw err;
                                 }
-                            }
-                        );
+                            });
                     }
                 })
                 .catch(err => {
                     if (err.code === 'ENOENT') {
-                        console.log(`No files inside of ${pack.name}`);
+                        console.log(`No files inside of ${pack}`);
                     } else {
                         throw err;
                     }
                 });
 
             extractPack(
-                `${MODULE_ID}/packs/${pack.name}`,
-                `${MODULE_ID}/src/packs/${pack.name}`,
+                `${SYSTEM_ID}/packs/${pack}`,
+                `${SYSTEM_ID}/src/packs/${pack}`,
                 {
                     yaml: true,
-                    transformName,
-                }
-            )
-            .catch(err => {
-                console.log(`Error extracting ${pack.name}: ${err}`);
-            });
+                    transform: transform,
+                })
+                .catch(err => {
+                    console.log(`Error extracting ${pack}: ${err}`);
+                });
         }
     });
 
@@ -57,7 +159,7 @@ fs.readdir(`${MODULE_ID}/packs`, {withFileTypes: true, recursive: true})
  * Prefaces the document with its type
  * @param {object} doc - The document data
  */
-function transformName(doc) {
+function transform(doc) {
     const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, '_');
     const type = doc._key.split('!')[1];
     const prefix = [ 'actors', 'items' ].includes(type) ? doc.type : type;
