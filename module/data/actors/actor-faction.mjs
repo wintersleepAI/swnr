85e72be9ad84d08ff32da8959e526520ee1e95e7
Jonah Middleton <jonah.middleton@gmail.com>
2025-02-19 20:24:26 -0500

    Define schema for faction actor

--- module/data/actors/actor-faction.mjs
+++ module/data/actors/actor-faction.mjs
@@ -1,15 +1,45 @@
-import SWNActorBase from './base-actor.mjs';
+import SWNShared from "../shared.mjs";
 
-export default class SWNFaction extends SWNActorBase {
+export default class SWNFaction extends foundry.abstract
+    .TypeDataModel {
   static LOCALIZATION_PREFIXES = [
-    ...super.LOCALIZATION_PREFIXES,
+    'SWN.Actor.base',
     'SWN.Actor.Faction',
   ];
 
   static defineSchema() {
     const fields = foundry.data.fields;
-    const schema = super.defineSchema();
+    const schema = {};
+    
+    schema.description = new fields.HTMLField();
+    schema.active = new fields.BooleanField({initial: true});
+    schema.health = new fields.SchemaField({
+      value: SWNShared.requiredNumber(7),
+      temp: SWNShared.requiredNumber(0)
+    });
+    schema.facCreds = SWNShared.requiredNumber(0);
+    schema.xp = SWNShared.requiredNumber(0);
+    schema.homeworld = SWNShared.requiredString("");
+    
+    schema.forceRating = SWNShared.requiredNumber(1);
+    schema.cunningRating = SWNShared.requiredNumber(1);
+    schema.wealthRating = SWNShared.requiredNumber(1);
 
+    schema.factionGoal = SWNShared.requiredString("");
+    schema.factionGoalDesc = SWNShared.requiredString("");
+    
+    schema.tags = new fields.ArrayField(
+        new fields.SchemaField({
+          name: SWNShared.requiredString(""),
+          desc: SWNShared.requiredString(""),
+          effect: SWNShared.requiredString("")
+        })
+    );
+    
+    schema.log = new fields.ArrayField(
+        new fields.StringField()
+    )
+    
     return schema;
   }
 
